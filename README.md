86b4365c02a6fe4e1411c5730a513f69c8961bd8
peter.nortey <peter.nortey@azubiafrica.org>
2021-08-12 14:00:41 +0000

    yargs part II

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # node-argv
+
+Trying out Node Argv
+
+Creating Commands and parsing with yargs
--- app.js
+++ app.js
@@ -10,8 +10,21 @@ yargs.version("1.1.0");
 yargs.command({
   command: "add",
   describe: "Add a new note",
-  handler: function () {
-    console.log("adding a new note");
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title:", argv.title);
+    console.log("Body:", argv.body);
   },
 });
 
@@ -43,4 +56,4 @@ yargs.command({
 });
 
 //add, remove, read, list
-console.log(yargs.argv);
+yargs.parse();
